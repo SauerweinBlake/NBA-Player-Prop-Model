e98997bdfa7dae2f6ec372019a0406a3b2effa5d
Blake Sauerwein <sauerwba@mail.uc.edu>
2023-02-09 20:10:04 -0500

    Refactored Player Prop Scraping Script

--- NBA_Player_Prop_Scraping.py
+++ NBA_Player_Prop_Scraping.py
@@ -13,6 +13,28 @@ from datetime import timedelta
 import re
 from selenium.webdriver.common.by import By
 
+def Stat_Switch_Case(stat):
+    if stat == 'Pts':
+        return 'PTS_PROP'
+    elif stat == 'Reb':
+        return 'REB_PROP'
+    elif stat == 'Ast':
+        return 'AST_PROP'
+    elif stat == '3pts':
+        return '3PTS_PROP'
+    elif stat == 'Blk':
+        return 'BLK_PROP'
+    elif stat == 'Stl':
+        return 'STL_PROP'
+    elif stat == 'Pts + Ast + Reb':
+        return 'PRA_PROP'
+    elif stat == 'Pts + Ast':
+        return 'PA_PROP'
+    elif stat == 'Pts + Reb':
+        return 'PR_PROP'
+    elif stat == 'Reb + Ast':
+        return 'RA_PROP'
+
 #%%
 # See if there is pre-existing data for Player Props
 try:
@@ -41,155 +63,45 @@ for _date_ in dates:
     DRIVER.get(URL)
     WebDriverWait(DRIVER, 30).until(EC.presence_of_element_located((By.XPATH,"/html/body/div[2]/main/div/div/div[1]/div[2]/div[1]/div")))
     time.sleep(2)
-# Find the number of pages of props for that specific Date
-    pages = DRIVER.find_element(By.XPATH, '//*[@id="props-app"]/div/div[2]/span').text.split(' ')[-1]
-    
+
+    # Find the number of pages of props for that specific Date
+    pages = int(DRIVER.find_element(By.XPATH, '//*[@id="props-app"]/div/div[2]/span').text.split(' ')[-1])
+
+    for ref in range(pages-1):
+        found_props = DRIVER.find_elements(By.ID, 'primary-info-container')
+        for player_prop_row in found_props:
+            player_info = player_prop_row.find_elements(By.XPATH, './child::*')[0].text
+            player_prop_info = player_prop_row.find_elements(By.XPATH, './child::*')[1].text
+
+            # Retrieve all necessary data
+            matchup = player_info.split('\n')[0]
+            player = player_info.split('\n')[1]
+            teams = [matchup.split(' ')[0], matchup.split(' ')[-1]]
+            player_team = player_info.split('\n')[2].split(' ')[0]
+            opp_team = teams[0] if player_team == teams[1] else teams[1]
+            val = player_prop_info.split('\n')[0]
+            stat = Stat_Switch_Case(player_prop_info.split('\n')[1])
+
+            # Clean/Manipulate Data
+            matchup = f'{teams[0]} @ {teams[1]}' if player_team == teams[0] else f'{teams[1]} vs. {teams[0]}'
+
+            # If there is not a row for a player on the date, create one
+            if (MASTER[(MASTER['GAME_DATE'] == _date_) & (MASTER['Player_Name'] == player)]).empty:
+                MASTER.loc[len(MASTER)] = [_date_, player, matchup, player_team, opp_team, 
+                                            np.nan, np.nan, np.nan, np.nan, np.nan,
+                                            np.nan, np.nan, np.nan, np.nan, np.nan]
+            
+            idx = MASTER[(MASTER['GAME_DATE'] == _date_) & (MASTER['Player_Name'] == player)].index.tolist()
+            if len(idx) > 1:
+                print('NO WORK MORE THAN 1 IDX')
+            else:
+                MASTER.at[idx[0], stat] = val
+
+        DRIVER.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        DRIVER.find_element(By.XPATH, '//*[@id="props-app"]/div/div[2]/button[2]/span/i').click()
+
 #%%
-    full_day_props = pd.DataFrame(columns=['date','player_name','stat','val'])
-    temp_df = pd.DataFrame(columns=['date','player_name','stat','val'])
-    if pages != 0:
-        for pag_num in range(1,pages+1,1):
-            found_props = DRIVER.find_elements(By.ID, 'primary-info-container')
-            for player_prop_row in found_props:
-                player_prop_row.findElements(By.XPATH, './child::*')
+MASTER.to_excel('Excels/Player_Prop_Data.xlsx')
+MASTER.to_csv('CSVs/Player_Prop_Data.csv')
+
 #%%
-
-
-            time.sleep(2)
-            page_html = DRIVER.page_source
-            page_soup = BeautifulSoup(page_html, 'html.parser')
-            a = page_soup.find_all("a")
-            players = [x.text.replace('\n','').strip() for x in a if '/nba/odds/player-props/' in x['href']]
-            players = [x for x in players if x != 'Player Props']
-            
-            mydivs = page_soup.find_all("div", "class" == "flex card__prop-container")
-            children = []
-            
-            pts = []
-            reb = []
-            ast = []
-            tpm = []
-            stl = []
-            blk = []
-            pra = []
-            pa = []
-            pr = []
-            ra = []
-            
-            props = [pts, reb, ast, tpm, stl, blk, pra, pa, pr, ra]
-            
-            val = []
-            stat = []
-            for x in mydivs:
-                children.append(x.findChildren("span"))
-                
-            for t in children:
-                pts.append(re.findall(">Pts</span>", str(t)))
-                reb.append(re.findall(">Reb</span>", str(t)))
-                ast.append(re.findall(">Ast</span>", str(t)))
-                tpm.append(re.findall(">3pts</span>", str(t)))
-                stl.append(re.findall(">Stl</span>", str(t)))
-                blk.append(re.findall(">Blk</span>", str(t)))
-                pra.append(re.findall(">Pts...Ast...Reb</span>", str(t)))
-                pa.append(re.findall(">Pts...Ast</span>", str(t)))
-                pr.append(re.findall(">Pts...Reb</span>", str(t)))
-                ra.append(re.findall(">Reb...Ast</span", str(t)))
-                val.append(re.findall(".[0-9].[0-9]</span>", str(t)))
-                
-            for i in range(len(pts)):
-                for j in range(len(props)):
-                    if props[j][i] != []:
-                        stat.append(props[j][i])
-            
-            val = [w for w in val if len(w) == 3]
-            
-            temp = []
-            for i in val:
-                temp.append(i[0].split('<')[0].split('>')[-1])
-            val = temp
-            
-            stat = [w[0].split('<')[0].split('>')[-1] for w in stat if len(w) == 1]
-            num_plays = len(val)
-            stat = stat[len(stat)-(num_plays*4):]
-            
-            temp = []
-            for i in range(0,len(stat),4):
-                temp.append(stat[i])
-            stat = temp
-            
-            temp_df = pd.DataFrame(columns=['DATE','PLAYER_NAME','STAT','VAL'])
-            temp_df['PLAYER_NAME'] = players
-            temp_df['STAT'] = stat
-            temp_df['VAL'] = val
-            temp_df['DATE'] = pd.to_datetime(str(_date_).split(' ')[0])
-            
-            if pag_num != pages:
-                DRIVER.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                next_page = DRIVER.find_element(By.XPATH,"/html/body/div[2]/main/div/div/div[2]/button[2]")
-                next_page.click()
-                
-            full_day_props = pd.concat([full_day_props, temp_df], axis=0, ignore_index=True)
-                
-        uniq_players = full_day_props['PLAYER_NAME'].unique()
-        for player in uniq_players:
-            try:
-                pts_prop = full_day_props[(full_day_props['PLAYER_NAME'] == player) & (full_day_props['STAT'] == 'Pts')]['VAL'].iloc[0]
-            except:
-                pts_prop = np.nan
-                
-            try:
-                ast_prop = full_day_props[(full_day_props['PLAYER_NAME'] == player) & (full_day_props['STAT'] == 'Ast')]['VAL'].iloc[0]
-            except:
-                ast_prop = np.nan
-                
-            try:
-                reb_prop = full_day_props[(full_day_props['PLAYER_NAME'] == player) & (full_day_props['STAT'] == 'Reb')]['VAL'].iloc[0]
-            except:
-                reb_prop = np.nan
-                
-            try:
-                tpts_prop = full_day_props[(full_day_props['PLAYER_NAME'] == player) & (full_day_props['STAT'] == '3pts')]['VAL'].iloc[0]
-            except:
-                tpts_prop = np.nan
-                
-            try:
-                blk_prop = full_day_props[(full_day_props['PLAYER_NAME'] == player) & (full_day_props['STAT'] == 'Blk')]['VAL'].iloc[0]
-            except:
-                blk_prop = np.nan
-                
-            try:
-                stl_prop = full_day_props[(full_day_props['PLAYER_NAME'] == player) & (full_day_props['STAT'] == 'Stl')]['VAL'].iloc[0]
-            except:
-                stl_prop = np.nan
-                
-            try:
-                pra_prop = full_day_props[(full_day_props['PLAYER_NAME'] == player) & (full_day_props['STAT'] == 'Pts + Reb + Ast')]['VAL'].iloc[0]
-            except:
-                pra_prop = np.nan
-                
-            try:
-                pa_prop = full_day_props[(full_day_props['PLAYER_NAME'] == player) & (full_day_props['STAT'] == 'Pts + Ast')]['VAL'].iloc[0]
-            except:
-                pa_prop = np.nan
-                
-            try:
-                pr_prop = full_day_props[(full_day_props['PLAYER_NAME'] == player) & (full_day_props['STAT'] == 'Pts + Reb')]['VAL'].iloc[0]
-            except:
-                pr_prop = np.nan
-                
-            try:
-                ra_prop = full_day_props[(full_day_props['PLAYER_NAME'] == player) & (full_day_props['STAT'] == 'Reb + Ast')]['VAL'].iloc[0]
-            except:
-                ra_prop = np.nan
-            
-            data = {'GAME_DATE': [_date_], 'Player_Name': [player], 'PTS_PROP': [pts_prop], 'AST_PROP': [ast_prop],
-                    'REB_PROP': [reb_prop], '3PTS_PROP': [tpts_prop], 'BLK_PROP': [blk_prop], 'STL_PROP': [stl_prop],
-                    'PRA_PROP': [pra_prop], 'PR_PROP': [pr_prop], 'PA_PROP': [pa_prop], 'RA_PROP': [ra_prop]}
-            
-            prop_df = pd.DataFrame(data)
-            
-            MASTER = pd.concat([MASTER, prop_df], axis=0, ignore_index=True)
-
-MASTER.to_excel('Player_Prop_Data.xlsx')
-
-# %%
