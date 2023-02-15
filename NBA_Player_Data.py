2fb2ec7b8ac1fb3285a88d1d40fecdb89a509d3a
Blake Sauerwein <sauerwba@mail.uc.edu>
2023-02-15 11:17:32 -0500

    Commit 2-13-23 Changes

--- NBA_Player_Data.py
+++ NBA_Player_Data.py
@@ -11,14 +11,6 @@ from nba_api.stats.library.parameters import SeasonAll
 from nba_api.stats.endpoints import commonplayerinfo
 
 #%%
-# Function to retrieve the Team ID based on the abbreviation
-def Retrieve_Team_ID(team_abbr):
-    try:
-        return teams.find_team_by_abbreviation(team_abbr)['id']
-    except:
-        return np.nan
-
-#%%
 # Create DataFrame and get list of active players and teams
 MASTER = pd.DataFrame()
 NBA_PLAYERS = [x for x in players.get_active_players()]
@@ -37,7 +29,6 @@ for player in NBA_PLAYERS:
         position = playerinfo['POSITION'].iloc[0]
         gamelog['Player_Name'] = player_name
         gamelog['POS'] = position
-        gamelog['TEAM'] = [x.split(' ')[0] for x in gamelog['MATCHUP']]
         gamelog['GAME_DATE'] = pd.to_datetime(gamelog['GAME_DATE'])
         gamelog.sort_values('GAME_DATE', ascending=True, inplace=True)
         gamelog.reset_index(inplace=True, drop=True)
@@ -51,30 +42,14 @@ for player in NBA_PLAYERS:
         continue
     # If the gamelog is not empty, create a dataframe for the next game
     else:
-        next_game_df = pd.DataFrame([[None]*len(gamelog.columns)], columns=gamelog.columns)
-        next_game_df['SEASON_ID'] = gamelog['SEASON_ID'].iloc[-1]
-        next_game_df['GAME_DATE'] = pd.to_datetime(date.today())
-        next_game_df['Player_ID'] = player_id
-        next_game_df['Player_Name'] = player_name
-        next_game_df['TEAM'] = gamelog['TEAM'].iloc[-1]
-        next_game_df['POS'] = position
-        
-        gamelog = pd.concat([gamelog, next_game_df], axis=0, ignore_index=True)
-        
+        # OFF DAYS DONE TWICE, HERE and DATA_CREATION
         gamelog['Off_Days'] = gamelog["GAME_DATE"].diff().apply(lambda x: x/np.timedelta64(1, 'D')).fillna(0).astype('int64')
         gamelog['Off_Days'] = [0 if x > 100 else x for x in gamelog['Off_Days']]
-        gamelog['TEAM_ID'] = [Retrieve_Team_ID(x) for x in gamelog['TEAM']]
         
         MASTER = pd.concat([MASTER, gamelog], axis=0, ignore_index=True)
         
         print("COMPLETE...",end='')
         
-# MASTER['POS_CODE'] = MASTER['POS'].astype('category').cat.codes
-# MASTER['TEAM_ID'] = [Retrieve_Team_ID(x) for x in MASTER['TEAM']]
-# MASTER['OPP'] = [x.split(' ')[2] for x in MASTER['MATCHUP']]
-# MASTER['OPP_ID'] = [Retrieve_Team_ID(x) for x in MASTER['OPP']]
-# MASTER['HA'] = [1 if x.split(' ')[1] =='@' else 0 for x in MASTER['MATCHUP']]
-# MASTER['WL'] = [1 if x == 'W' else 0 for x in MASTER['WL']]
 MASTER.drop(['VIDEO_AVAILABLE', 'FG_PCT', 'FG3_PCT', 'FT_PCT'], axis=1, inplace=True)
 
 #%%
